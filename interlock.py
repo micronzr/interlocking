a8a55a720bb922071935ae4a1cd816f7633d5897
phae <phaedra042@gmail.com>
2024-02-08 08:28:19 +1300

    plan a

---
+++ interlock.py
@@ -0,0 +1,93 @@
+# imports
+
+# switch class
+
+# functions
+#init
+ # clear required and blocked and linked signal/point
+ 
+# set linked signal/point
+# get linked signal/point
+ 
+#set position
+  #read pin
+  #check last position
+  #set didchange
+  #current pos to last pos
+   #if set
+  # while requires
+    #check set
+    # if set
+    # position set
+    # else
+      # position error
+   #while blocked 
+     #IF Set
+     #position error
+    #else
+    # position set
+    #else (not set)
+    # position unset
+    
+#didchange
+    
+#set pin    
+  # gpio pin  
+    
+    
+#set requires
+  #add switch
+#set blocked by
+  #add switch
+#get position
+  #return position
+
+#-------- end of class
+
+# class point
+# set point position
+
+#------- end of class
+
+# class signal
+#set signal position
+#----- end of class
+
+
+# create switches
+# create points
+# create signals
+
+#assign points to switches
+#assign signals to switches
+#switch#.set-requires
+#switch#.set-blocked
+
+
+
+
+# frame list of switches
+
+
+
+#error=0
+
+# while true
+#while item in frame
+#if error=0    (only do until first error)
+#item.setposition()
+#if item.didchange.
+#then
+#item.getpoint().setposition(item.getposition)
+#if item.getposition=error 
+  #then error =1 
+
+    #while error = 1
+    #error= 0
+     #while item in frame
+     # item.setposition
+     # if  item.getposition = error 
+      # then error = 1
+
+
+  
